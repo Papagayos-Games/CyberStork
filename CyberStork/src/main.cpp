43f562a959fa0cff9ad79b20c822c61c8f95609b
Stiven <starias@ucm.es>
2021-05-06 16:52:14 +0200

    Proyecto CyberStork iniciado

---
+++ CyberStork/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+//#include "PapagayoEngine.h"
+
+/*#if (defined _DEBUG) || !(defined _WIN32)
+int main() {
+
+#else
+#include <Windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+	*/
+
+
+int main() {
+//#ifdef _DEBUG
+//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//#endif
+	//try {
+	//	PapagayoEngine::getInstance()->run();
+	//	PapagayoEngine::getInstance()->clean();
+	//}
+	//catch (std::exception e) {
+	//	std::cout << e.what() << "\n";
+	//}
+	//catch (const std::string& e) {
+	//	std::cout << e << "\n";
+	//}
+	//catch (...) {
+	//	std::cout << "EXCEPCION NO CONTROLADA\n";
+	//}
+	//
+	//return 0;
+}

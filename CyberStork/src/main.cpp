f32de0b80a5c7b7ad69e9abb4ea73c46b1a6bb19
Pablo Rodriguez Bobada <prodra02@ucm.es>
2021-05-10 12:56:08 +0200

    Primeras opciones del proyecto

--- CyberStork/src/main.cpp
+++ CyberStork/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include "PapagayoEngine.h"
+#include "PapagayoEngine.h"
 
 /*#if (defined _DEBUG) || !(defined _WIN32)
 int main() {
@@ -13,22 +13,22 @@ WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdS
 
 
 int main() {
-//#ifdef _DEBUG
-//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-//#endif
-	//try {
-	//	PapagayoEngine::getInstance()->run();
-	//	PapagayoEngine::getInstance()->clean();
-	//}
-	//catch (std::exception e) {
-	//	std::cout << e.what() << "\n";
-	//}
-	//catch (const std::string& e) {
-	//	std::cout << e << "\n";
-	//}
-	//catch (...) {
-	//	std::cout << "EXCEPCION NO CONTROLADA\n";
-	//}
-	//
-	//return 0;
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	try {
+		PapagayoEngine::getInstance()->run();
+		PapagayoEngine::getInstance()->clean();
+	}
+	catch (std::exception e) {
+		std::cout << e.what() << "\n";
+	}
+	catch (const std::string& e) {
+		std::cout << e << "\n";
+	}
+	catch (...) {
+		std::cout << "EXCEPCION NO CONTROLADA\n";
+	}
+	
+	return 0;
 }

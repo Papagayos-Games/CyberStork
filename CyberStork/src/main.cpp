4534ce781c71ae5ce91df33991ad66aac71aa35b
PabloRBGM <prodra02@ucm.es>
2021-05-18 20:28:16 +0200

    MOD: el nombre de la ventana

--- CyberStork/src/main.cpp
+++ CyberStork/src/main.cpp
@@ -17,7 +17,7 @@ int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 	try {
-		if (!PapagayoEngine::setupInstance("PAPAGAYO ENGINE"))
+		if (!PapagayoEngine::setupInstance("CyberStork"))
 			throw std::exception("Couldn't Initialize Papagayo Engine\n");
 		PapagayoEngine::getInstance()->init();
 	}

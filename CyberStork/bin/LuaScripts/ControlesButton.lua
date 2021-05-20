ce4f5a6071edd25f952a7d1e798bccfc79fbcaae
raulblas7 <48948743+raulblas7@users.noreply.github.com>
2021-05-20 23:29:49 +0200

    ADD: Funcionalidad del boton de controles hecha
    Además de la escena controles, y de un boton para volver al menu principal

---
+++ CyberStork/bin/LuaScripts/ControlesButton.lua
@@ -0,0 +1,23 @@
+local UIclase = {}
+
+UIclase["instantiate"] = function (params, entity)
+    --p = JSON:decode(params)
+    local self = {}
+    self.entity = entity
+    return self
+end
+
+UIclase["start"] = function (_self, lua)
+    _self.button = lua:getUIButton(_self.entity)
+end
+
+UIclase["update"] = function (_self, lua)
+    if _self.button:getButtonPressed() == true then
+        lua:changeScene("controls")
+        _self.button:buttonNotPressed()
+    end
+
+
+end
+
+return UIclase
